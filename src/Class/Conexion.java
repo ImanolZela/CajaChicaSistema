4a95b3d5324ce6109d86778b2a5586bdb3ca1725
ImanolZela <imanolza1304@gmail.com>
2024-10-05 01:07:24 -0500

    firstCommit

---
+++ src/Class/Conexion.java
@@ -0,0 +1,39 @@
+
+package Class;
+
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Conexion {
+    private static Connection conn = null;
+    private static String url = "jdbc:sqlite:D:/ProyectoAlgoritmos/CajaChicaSistema/src/sistemacajachica.sqlite";
+    private static String driver = "org.sqlite.JDBC";
+
+    public static Connection conectar() {
+        try {
+            if (conn == null) {
+                Class.forName(driver);
+                conn = DriverManager.getConnection(url);
+                System.out.println("Conexión establecida a la base de datos SQLite");
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Error al conectar a la base de datos: " + e.getMessage());
+        }
+        return conn;
+    }
+
+    public static void desconectar() {
+        try {
+            if (conn != null) {
+                conn.close();
+                System.out.println("Conexión cerrada");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al cerrar la conexión: " + e.getMessage());
+        }
+    }
+}
---
+++ src/Model/GastoCategoria/CategoriaGastoModel.java
@@ -0,0 +1,6 @@
+
+package Model.GastoCategoria;
+
+public class CategoriaGastoModel {
+    
+}
